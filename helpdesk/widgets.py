314fd9fff2c323e03a88f596d3d29c5ef99bc2b1
Amidu Dabor <36164952+Amidu-Dabor@users.noreply.github.com>
2023-12-25 02:09:34 +0300

    Delete helpdesk directory

--- helpdesk/widgets.py
+++
@@ -1,7 +0,0 @@
-# # widgets.py
-# from django import forms
-# from django.forms.widgets import RadioFieldRenderer
-#
-#
-# class RadioFieldListEditableWidget(forms.CheckboxSelectMultiple):
-#     renderer = RadioFieldRenderer
