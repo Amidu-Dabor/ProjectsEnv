110f33255b7d337e7f14942d53c7b0b5052edbfd
Amidu Dabor <36164952+Amidu-Dabor@users.noreply.github.com>
2025-03-25 01:52:35 +0300

    Updated modal_deploy.yml

--- modal_deploy.py
+++ modal_deploy.py
@@ -2,7 +2,7 @@ import modal
 
 stub = modal.Stub("chatbot_prototype")
 
-# Images for each microservice
+# Docker images for each microservice
 api_gateway_image = modal.Image.from_dockerhub("amidu/chatbot_prototype_api_gateway:latest")
 auth_service_image = modal.Image.from_dockerhub("amidu/chatbot_prototype_auth_service:latest")
 data_service_image = modal.Image.from_dockerhub("amidu/chatbot_prototype_data_service:latest")
